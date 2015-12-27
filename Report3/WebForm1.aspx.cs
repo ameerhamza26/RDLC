267663d49d73735497c04647681d8702a36668fd
ausis26 <ameerhamza810@gmail.com>
2015-12-27 23:17:50 +0500

    Commit

---
+++ Report3/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WebForms;
+
+namespace Report3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Report1.rdlc");
+                DataSet1 dsCustomers = GetData();
+                ReportDataSource datasource = new ReportDataSource("DataSet1", dsCustomers.Tables[0]);
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(datasource);
+            }
+
+        }
+
+        private DataSet1 GetData()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["threetier"].ConnectionString;
+            SqlCommand cmd = new SqlCommand("BasicInfo_GetBasicInformation");
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sda.SelectCommand = cmd;
+                    using (DataSet1 dsCustomers = new DataSet1())
+                    {
+                        sda.Fill(dsCustomers, "DataTable1");
+                        return dsCustomers;
+                    }
+                }
+            }
+        }
+    }
+}
